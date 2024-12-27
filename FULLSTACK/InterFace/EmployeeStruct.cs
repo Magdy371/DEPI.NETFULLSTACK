12ae3334c96d31f5a176d828db11ea1ddce9bb81
Magdy Elshrief <magdyelshrief37@gmail.com>
2024-12-27 10:38:08 +0200

    pass by value and pass by reference

---
+++ FULLSTACK/InterFace/EmployeeStruct.cs
@@ -0,0 +1,15 @@
+namespace InterFace
+{
+    public struct EmployeeStruct
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+
+        public void EmployeeInfos()
+        {
+            System.Console.WriteLine($"you full infos are: {FirstName} {LastName} {Address} {City}");
+        }
+    }
+}
--- FULLSTACK/InterFace/Program.cs
+++ FULLSTACK/InterFace/Program.cs
@@ -20,6 +20,34 @@
             employee.name = "John";
             employee.age = 30;
             employee.getInfos();
+
+            //pass by value vs pass by reference
+            //pass by value: when you pass a value type as a parameter to a method, 
+            //a new copy of the value is created.
+            //pass by reference: when you pass a reference
+            //type as a parameter to a method, a reference to the object is passed.
+            #region pass by value vs pass by reference
+            Employee emp2 = new Employee();
+            emp2 = employee;
+            emp2.name = "Doe";
+            emp2.age = 40;
+            employee.getInfos();
+
+            EmployeeStruct empStruct = new EmployeeStruct();
+            empStruct.FirstName = "John";
+            empStruct.LastName = "Doe";
+            empStruct.Address = "123 Main St";
+            empStruct.City = "New York";
+            empStruct.EmployeeInfos();
+
+            EmployeeStruct empStruct2 = new EmployeeStruct();
+            empStruct2 = empStruct;
+            empStruct2.FirstName = "Jane";
+            empStruct2.LastName = "Smith";
+            empStruct2.Address = "456 Main St";
+            empStruct2.City = "Los Angeles";
+            empStruct.EmployeeInfos();
+            #endregion
         }
     }
 }
