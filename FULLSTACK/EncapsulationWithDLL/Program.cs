5fcb263965f24d404e1fb68b27ccca79bdc877fe
Magdy Elshrief <magdyelshrief37@gmail.com>
2024-12-28 19:35:49 +0200

    Test Access modifiers out of the namespace

--- FULLSTACK/EncapsulationWithDLL/Program.cs
+++ FULLSTACK/EncapsulationWithDLL/Program.cs
@@ -12,6 +12,17 @@ namespace EncapsulationWithDLL
             System.Console.WriteLine(em.subtract(a,b));
             System.Console.WriteLine(em.multiply(a,b));
             System.Console.WriteLine(em.divide(a,b));
+
+            AccessModifiers obj = new AccessModifiers();
+            obj.publicProperty = "Public Property Accessed";
+            /*
+            other access modifeirs cannot be accessed because
+            1- Internal is out from the namespace
+            2-Private is not in the same class
+            3-private protected is not in the same calss or in heritance
+            4-internal protected as it is not in the same name  space or inherited
+            */
         }
+        /*class access modifiers can not be other than public or internal*/
     }
 }
