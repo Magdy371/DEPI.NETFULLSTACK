79f99f09561f78e99bac414fb1a3628fa4587eed
Magdy Elshrief <magdyelshrief37@gmail.com>
2024-12-29 01:24:10 +0200

    Linq definition

---
+++ EF CORE/EF-Core/LinqQuery/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace LinqQuery
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            /*
+             * Linq is language IntegratedQuery
+             * Linq provide intellisense and compile time error checking
+             * Linq Query Types:
+             *  1- Object Collection=>(Array , list)
+             *  2- Linq to dataset (Ado.net =>used for dealing with database)
+             *  3- Linq to XML (XML Documents) => xml used to describe data
+             *      XML IS Media Data Source
+             *  4- Linq to Entities EF CORE (ORM)
+             *  5- Linq to SQL depricated
+             *  6- Linq to IQueryable (other data sources)
+             */
+
+
+        }
+    }
+}
