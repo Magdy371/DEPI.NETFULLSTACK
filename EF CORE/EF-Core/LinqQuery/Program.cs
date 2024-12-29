18c9538b7cf4aef522845eeb513dc312999167c5
Magdy Elshrief <magdyelshrief37@gmail.com>
2024-12-29 09:22:41 +0200

    Code Enhanced

--- EF CORE/EF-Core/LinqQuery/Program.cs
+++ EF CORE/EF-Core/LinqQuery/Program.cs
@@ -6,84 +6,89 @@
         public static void Main(string[] args)
         {
             /*
-             * Linq is language IntegratedQuery
-             * Linq provide intellisense and compile time error checking
-             * Linq Query Types:
-             *  1- Object Collection=>(Array , list)
-             *  2- Linq to dataset (Ado.net =>used for dealing with database)
-             *  3- Linq to XML (XML Documents) => xml used to describe data
-             *      XML IS Media Data Source
-             *  4- Linq to Entities EF CORE (ORM)
-             *  5- Linq to SQL depricated
-             *  6- Linq to IQueryable (other data sources)
+             * LINQ (Language Integrated Query)
+             * LINQ provides IntelliSense and compile-time error checking.
+             * LINQ Query Types:
+             *  1- Object Collection (Array, List)
+             *  2- LINQ to Dataset (ADO.NET - used for dealing with database)
+             *  3- LINQ to XML (XML Documents) - XML is used to describe data
+             *  4- LINQ to Entities (EF Core - ORM)
+             *  5- LINQ to SQL (deprecated)
+             *  6- LINQ to IQueryable (other data sources)
              */
-            List<string> names = new List<string>();
-            names.Add("Ahmed");
-            names.Add("Sayed");
-            names.Add("magdy");
+            List<string> names = new List<string> { "Ahmed", "Sayed", "Magdy" };
 
-            //Select name that stars with m
-            foreach(var name in names)
+            // Select names that start with 'm' using a foreach loop
+            foreach (var name in names)
             {
-                if(name.StartsWith("m"))
+                if (name.StartsWith("M", StringComparison.OrdinalIgnoreCase))
                 {
                     Console.WriteLine(name);
                 }
             }
-            //Select name that stars with m with linq
-            //Linq to object
-            var nameM = names.Where(names => names.StartsWith("m")).First();
+
+            // Select name that starts with 'm' using LINQ
+            var nameM = names.FirstOrDefault(n => n.StartsWith("M", StringComparison.OrdinalIgnoreCase));
             Console.WriteLine(nameM);
 
-            //how to intialize object
-            //cannot be done without default constructor
-            Info inf = new Info() { name = "Ahmed", description = "Senior developer" };
+            // Initializing objects
+            var inf = new Info { Name = "Ahmed", Description = "Senior Developer" };
 
-            List<Info> infos = new List<Info>();
-            infos.Add(inf);
-            infos.Add(new Info() { name = "Rady", description = "director" });
-            infos.Add(new Info("Magdy", "Senior Developer"));
-            infos.Add(new Info("Menna", "Jonior Developer"));
-
-            var result = infos.Where(infos => infos.name.Contains("m")).First();
-            Console.WriteLine($"Result is {result.name}");
-            //Or we can use this 
-            var fresult  = from emp in infos
-                           where emp.name.Contains("M")
-                           select emp;
-            foreach(var item in fresult)
-            {
-                Console.WriteLine($"names contaisn m letter are {item.name}");
-            }
-
-            List < Info > oInfos = new List<Info>
+            var infos = new List<Info>
             {
                 inf,
-                new Info() { name = "Ahmed", description = "Senior developer" },
-                new Info() { name = "Magdy", description = "CEO" }
+                new Info { Name = "Rady", Description = "Director" },
+                new Info("Magdy", "Senior Developer"),
+                new Info("Menna", "Junior Developer")
             };
 
-            Console.WriteLine($"INDEX NUMBER 2 IN THE LIST IS {infos[2].name} --> {infos[2].description}");
-            foreach (var info in infos)
+            var result = infos.FirstOrDefault(i => i.Name?.Contains("m", StringComparison.OrdinalIgnoreCase) == true);
+            if (result != null)
             {
-                Console.WriteLine($"{info.name} --> {info.description}");
+                Console.WriteLine($"Result is {result.Name}");
             }
 
+            // LINQ query
+            var fresult = from emp in infos
+                          where emp.Name?.Contains("M", StringComparison.OrdinalIgnoreCase) == true
+                          select emp;
 
+            foreach (var item in fresult)
+            {
+                Console.WriteLine($"Names containing 'M' letter are {item.Name}");
+            }
+
+            var oInfos = new List<Info>
+            {
+                inf,
+                new Info { Name = "Ahmed", Description = "Senior Developer" },
+                new Info { Name = "Magdy", Description = "CEO" }
+            };
+
+            Console.WriteLine($"INDEX NUMBER 2 IN THE LIST IS {infos[2].Name} --> {infos[2].Description}");
+            foreach (var info in infos)
+            {
+                Console.WriteLine($"{info.Name} --> {info.Description}");
+            }
+
+            // Anonymous type does not require a blueprint (class)
+            // Anonymous type is read-only
+            var student = new { ID = 1, Name = "Magdy", Title = "Senior .NET Developer" };
+            Console.WriteLine($"Student: ID = {student.ID}, Name = {student.Name}, Title = {student.Title}");
         }
     }
+
     class Info
     {
-        public Info()
+        public Info() { }
+
+        public Info(string name, string title)
         {
-            
+            Name = name;
+            Description = title;
         }
-        public Info(string name , string title)
-        {
-            this.name = name;
-            description = title;
-        }
-        public string? name { get; set; }
-        public string? description { get; set; }
+
+        public string? Name { get; set; }
+        public string? Description { get; set; }
     }
 }
