cfd4c2ef3d6d037bdd49cc49224667edefe1ac15
Magdy Elshrief <magdyelshrief37@gmail.com>
2025-01-24 02:40:40 +0200

    Is Prime number check

---
+++ DSA/BasicProblems/PrimeNumbers.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicProblems
+{
+    internal class PrimeNumbers
+    {
+        public static string isPrime(int number)
+        {
+            if (number <= 1)
+                return "false: not prime number";
+            for (int i = 2; i <= Math.Sqrt(number); i++)
+            {
+                if (number % i == 0)
+                    return "false: not prime number";
+            }
+            return "true: its is a prime number";
+        }
+    }
+}
--- DSA/BasicProblems/Program.cs
+++ DSA/BasicProblems/Program.cs
@@ -6,6 +6,9 @@
         {
             Console.WriteLine(SumDigits.sumOfDigits(123));
             Console.WriteLine(SumDigits.sumOfDigitsIterative(123));
+            Console.WriteLine(SumDigits.sumOfDigits("123"));
+            Console.WriteLine("--------------Prime Number checking--------------");
+            Console.WriteLine(PrimeNumbers.isPrime(5));
         }
     }
 }
