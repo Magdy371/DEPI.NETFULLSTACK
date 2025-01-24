3b8d7aee474de27167871d07155c3b568974d11b
Magdy Elshrief <magdyelshrief37@gmail.com>
2025-01-24 03:06:23 +0200

    is numebr x can powered to be equal to number y

---
+++ DSA/BasicProblems/NumberPowerCheck.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicProblems
+{
+    internal class NumberPowerCheck
+    {
+        public static bool isPower(int x, int y)
+        {
+            double res1 = Math.Log(y) / Math.Log(x);
+
+            // Note : this is double 
+            double res2 = Math.Log(y) / Math.Log(x);
+            return (res1 == res2);
+
+        }
+    }
+}
--- DSA/BasicProblems/Program.cs
+++ DSA/BasicProblems/Program.cs
@@ -9,6 +9,10 @@
             Console.WriteLine(SumDigits.sumOfDigits("123"));
             Console.WriteLine("--------------Prime Number checking--------------");
             Console.WriteLine(PrimeNumbers.isPrime(5));
+            Console.WriteLine("--------------Power Number checking--------------");
+            Console.WriteLine(NumberPowerCheck.isPower(10, 1) ? "true" : "false");
+            Console.WriteLine(NumberPowerCheck.isPower(1, 20) ? "true" : "false");
+            Console.WriteLine(NumberPowerCheck.isPower(2, 128) ? "true" : "flase");
         }
     }
 }
