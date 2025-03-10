2b336c8c529781b3c644aee35b0f6962474ec2b3
Magdy Elshrief <magdyelshrief37@gmail.com>
2025-03-10 02:52:51 +0200

    Generics Done "Now Collections Entry"

---
+++ FULLSTACK/Generics/CustomerToPersonMapper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    public class CustomerToPersonMapper : IMapper<Person, Customer>
+    {
+        public Person Map(Customer source)
+        {
+            return new Person
+            {
+                Id = source.Id,
+                Name = source.Name,
+                Age = source.Age
+            };
+
+        }
+    }
+}
---
+++ FULLSTACK/Generics/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Generics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var Person1 = new Person { Id = 1, Name = "Magdy", Age = 25 };
+            var Person2 = new Person { Id = 2, Name = "Mohamed", Age = 22 };
+            Console.WriteLine($"Person1: Name->{Person1.Name} Age->{Person1.Age}");
+            Console.WriteLine($"Person2: Name->{Person2.Name} Age->{Person2.Age}");
+            Swap(ref Person1, ref Person2);
+            Console.WriteLine($"S.Person1: Name->{Person1.Name} Age->{Person1.Age}");
+            Console.WriteLine($"S.Person2: Name->{Person2.Name} Age->{Person2.Age}");
+
+
+            int x = 10, y = 70;
+            Console.WriteLine("Before Swapping x = {0} and y = {1}", x, y);
+            Swap(ref x, ref y);
+            Console.WriteLine("After Swapping x = {0} and y = {1}", x, y);
+
+            //Object Serialize
+            string jsonPerson = @"{
+            ""Id"":0 , 
+            ""Name"":""Matt"",
+            ""Age"":50
+            }";
+            Console.WriteLine("Desacralizing jsonPerson");
+            var PJ = System.Text.Json.JsonSerializer.Deserialize<Person>(jsonPerson);
+            Console.WriteLine($" {PJ?.Name} , {PJ?.Age}");
+
+            Console.WriteLine("Generic example");
+            Nullable<int> n = new Nullable<int>(5);
+            Console.WriteLine($"n value: {n.GetValueOrDefault()}");
+
+            Console.WriteLine("=====Mapper=====");
+            var customer  = new Customer { Id = 1, Name = "Magdy", Age = 25 };
+            var mapper = new CustomerToPersonMapper();
+            var person = mapper.Map(customer);
+            Console.WriteLine($"after Mapping Id:{person.Id}, Name: {person.Name}, Age: {person.Age}");
+        }
+        public static void Swap<T>(ref T ele1 , ref T ele2)
+        {
+            T temp = ele2;
+            ele2 = ele1;
+            ele1 = temp;
+        }
+    }
+}
